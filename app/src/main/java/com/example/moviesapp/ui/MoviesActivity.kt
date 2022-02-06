6f2ced3cabdffd3cf9889a418fef38cc03780b12
Evgeniy Moiseev <moiseev.evgeniy.1985@gmail.com>
2022-02-06 22:17:50 +0200

    landscape activity layout added

--- app/src/main/java/com/example/moviesapp/ui/MoviesActivity.kt
+++ app/src/main/java/com/example/moviesapp/ui/MoviesActivity.kt
@@ -2,8 +2,10 @@ package com.example.moviesapp.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.example.moviesapp.R
 import com.example.moviesapp.databinding.ActivityMoviesBinding
@@ -20,26 +22,39 @@ class MoviesActivity : AppCompatActivity() {
             .findFragmentById(R.id.fragmentContainer) as NavHostFragment
         val navController = navHostFragment.navController
 
-        binding.bottomNavigationView.setupWithNavController(navController)
+        binding.bottomNavigationView?.setupWithNavController(navController)
+
         navController.addOnDestinationChangedListener { _, destination, args ->
             when (destination.id) {
                 R.id.mostPopularMoviesFragment -> {
-                    binding.bottomNavigationView.visibility = View.VISIBLE
+                    binding.bottomNavigationView?.visibility = View.VISIBLE
                     supportActionBar?.title = getString(R.string.most_popular_movies)
                 }
                 R.id.favoriteMoviesFragment -> {
-                    binding.bottomNavigationView.visibility = View.VISIBLE
+                    binding.bottomNavigationView?.visibility = View.VISIBLE
                     supportActionBar?.title = getString(R.string.watch_list)
                 }
                 R.id.searchFragment -> {
-                    binding.bottomNavigationView.visibility = View.VISIBLE
+                    binding.bottomNavigationView?.visibility = View.VISIBLE
                     supportActionBar?.title = getString(R.string.search)
                 }
                 R.id.movieDetailFragment -> {
-                    binding.bottomNavigationView.visibility = View.GONE
+                    binding.bottomNavigationView?.visibility = View.GONE
                     supportActionBar?.title = args?.getString(TITLE_BUNDLE_KEY)
                 }
             }
+        }
+
+        if (binding.drawerLayout != null) {
+            binding.navigationView?.setupWithNavController(navController)
+            val toggle = ActionBarDrawerToggle(
+                this,
+                binding.drawerLayout,
+                R.string.directors,
+                R.string.stars
+            )
+            binding.drawerLayout?.addDrawerListener(toggle)
+            toggle.syncState()
         }
     }
 }
